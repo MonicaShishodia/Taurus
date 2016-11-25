9cffb7896c5cea2d7bf46a8550e8f97fadf169fb
MonicaShishodia <monicashishodia@gmail.com>
2016-11-25 10:09:14 +0100

    Add files via upload

---
+++ Eva_Login_Page.js
@@ -0,0 +1,18 @@
+var assert = require('assert'),
+test = require('C:/Users/eqruuwq/node_modules/selenium-webdriver/testing'),
+webdriver = require('C:/Users/eqruuwq/node_modules/selenium-webdriver');
+ 
+test.describe('Eva Login page', function() {
+  test.it('should work', function() {
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+driver.get('http://172.29.221.77');
+    var UserNameBox = driver.findElement(webdriver.By.name('UserName'));
+    UserNameBox.sendKeys('eqruuwq');
+    UserNameBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'eqruuwq');
+    });
+    driver.quit();
+  });
+});
